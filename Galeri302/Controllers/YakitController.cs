1ecef78f0ffe3a3e5721446a9f31ed560b1896c8
nazan <nazanposul@gmail.com>
2019-10-30 15:52:56 +0200

    Initial commit

---
+++ Galeri302/Controllers/YakitController.cs
@@ -0,0 +1,86 @@
+﻿using Galeri302.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Galeri302.Controllers
+{
+    public class YakitController : Controller
+    {
+        private G302Context db = new G302Context();
+        // GET: Yakit
+        public ActionResult Index()
+        {
+            return View(db.yYakitTipis.ToList());
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(yYakitTipi yakit)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.yYakitTipis.Add(yakit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            return View(yakit);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+           yYakitTipi yakit = db.yYakitTipis.Find(id);
+
+            if (yakit == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(yakit);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(yYakitTipi yakit)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.Entry(yakit).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            return View(yakit);
+        }
+
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            yYakitTipi yakit = await db.yYakitTipis.FindAsync(id);
+            if (yakit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(yakit);
+        }
+    }
+}
---
+++ Galeri302/Models/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Galeri302.Models
+{
+    public enum Status
+    {
+        Sıfır=0,
+        [Display(Name ="İkinci El")]
+        IkinciEl=1
+    }
+}
